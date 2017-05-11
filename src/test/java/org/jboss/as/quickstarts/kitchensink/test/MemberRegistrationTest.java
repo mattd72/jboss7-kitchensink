eea6cf3eba714e2674bb1bb6044cd4d917a20dcd
David La Motta <david@emitrom.com>
2017-05-11 19:08:25 -0400

    Update MemberRegistrationTest.java

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -17,40 +17,14 @@
 package org.jboss.as.quickstarts.kitchensink.test;
 
 import static org.junit.Assert.assertNotNull;
-
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.quickstarts.kitchensink.model.Member;
-import org.jboss.as.quickstarts.kitchensink.service.MemberRegistration;
-import org.jboss.as.quickstarts.kitchensink.util.Resources;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 public class MemberRegistrationTest {
 
-    @Inject
-    MemberRegistration memberRegistration;
-
-    @Inject
-    Logger log;
-
     @Test
     public void testRegister() throws Exception {
-        Member newMember = new Member();
-        newMember.setName("Jane Doe");
-        newMember.setEmail("jane@mailinator.com");
-        newMember.setPhoneNumber("2125551234");
-        memberRegistration.register(newMember);
-        assertNotNull(newMember.getId());
-        log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+        throw new Exception("Explicit test failure!");
     }
 
 }

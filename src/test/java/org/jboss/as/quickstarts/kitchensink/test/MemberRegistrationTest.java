0605e6d9635cfd604006f83dd168fdc94bddc3ce
matt <mattd@redhat.com>
2017-06-15 07:05:49 -0400

    Update MemberRegistrationTest.java

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -26,7 +26,7 @@ public class MemberRegistrationTest {
     public void testRegister() throws Exception {
         // No errors here. Uncomment next line to kill the build.
         // throw new Exception("Fat-fingered typo.");
-        System.out.println("Success!!");
+        System.out.println("Success!");
     }
 
 }
